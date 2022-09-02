b010902031273a8ae96ad181b4ec520dffc9d4a8
Carlos Alvarado Robles <carlos2000emilioa@gmail.com>
2022-09-02 11:11:24 -0500

    Añadiendo el Modelo ER del aplicativo

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('login');
 });
 
 Route::get('/dashboard', [IncidenteController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
