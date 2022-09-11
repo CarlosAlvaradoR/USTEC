1702f394e9c710c5db24c2a3c2ad7edc61b0779d
CARLOS ORELLANO <orellano428@gmail.com>
2022-09-10 20:08:17 -0500

    creacion de incidentes de tipo software

---
+++ app/Http/Livewire/CreateIncidenteSoftware.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Area;
+use Livewire\Component;
+use App\Models\Incidente;
+use App\Models\Importancia;
+
+class CreateIncidenteSoftware extends Component
+{
+    public $descripcion;
+    public $gravedad;
+    public $titulo;
+    public $area;
+    public $tipo;
+
+    protected $rules = [
+        'descripcion' => 'required|string',
+        'area' => 'required',
+        'gravedad' => 'required',
+        'titulo' => 'required|string'
+    ];
+    public function mount()
+    {
+        //  $this->equipo = $equipo;
+        $this->tipo = 2;
+    }
+    public function crearIncidente()
+    {
+        $datos = $this->validate();
+        //s  dd($this->equipo->id . ' ' . $this->tipo);
+        Incidente::create([
+
+            'descripcion' => $datos['descripcion'],
+            'titulo' => $datos['titulo'],
+            // ** se dejo de usar area porque el equipo ya tiene la asociacion 
+            // 'area_id' => $datos['area'], 
+            'importancia_id' => $datos['gravedad'],
+            'tipo_id' =>  $this->tipo,
+            // 'equipo_id' => $this->equipo->id,
+            'user_id' => auth()->user()->id
+        ]);
+
+
+        //Crear un Mensaje
+        session()->flash('mensaje-incidente', 'El incidente software se guardo correctamente');
+
+        //redireccionar al usuario
+        return redirect()->route('incidentes.create', 'software');
+    }
+
+
+
+
+    public function render()
+    {
+        $gravedades = Importancia::all();
+        $areas = Area::all();
+        return view('livewire.create-incidente-software', [
+            'areas' => $areas,
+            'gravedades' => $gravedades
+        ]);
+    }
+}
--- resources/views/incidentes/create-software.blade.php
+++ resources/views/incidentes/create-software.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Crear Incidente') }}
+            {{ __('Crear Incidente Software') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <h2>Soft</h2>
+                    <livewire:create-incidente-software />
                 </div>
             </div>
         </div>
