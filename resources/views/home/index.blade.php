6417bf555aa5eefc8e362e36084cf884c932a1b4
Carlos Orellano <orellano428@gmail.com>
2022-10-19 19:23:02 -0500

    arreglos en home

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,17 +4,18 @@
             {{ __('Consulta') }}
         </h2>
     </x-slot>
-    <div class="py-16 bg-gray-50 overflow-hidden lg:py-24">
+    <div class=" bg-gray-50 overflow-hidden lg:py-24 bg-[url('../img/back.jpg')] bg-no-repeat bg-cover ">
         <div class=" max-w-xl mx-auto px-4 sm:px-6 lg:px-8 lg:max-w-7xl">
-            <div class="relative">
+            <div class="relative backdrop-blur-sm bg-white/30 p-5 mt-3">
+
                 <h2 class="text-center text-4xl leading-8 font-extrabold tracking-tight text-blue-700 sm:text-6xl">
                     Consulta el estado de tu equipo</h2>
-                <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-500">Conoce mas sobre el historial de
+                <p class="mt-4 max-w-3xl mx-auto text-center text-xl text-gray-50">Conoce mas sobre el historial de
                     problemas y las soluciones que puedes usar ahora</p>
-                <div class="grid place-items-center mt-3">
+
+                <div class="grid place-items-center mt-10">
                     <livewire:historial-incidencias>
                 </div>
-
 
             </div>
 
